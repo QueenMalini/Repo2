b55827ea32409d56e4bf37884628981d7fb624af
QueenMalini <113904638+QueenMalini@users.noreply.github.com>
2022-12-09 15:20:38 +0530

    chnage code
    plz review

--- Sample/src/samplePkg/Sample1.java
+++ Sample/src/samplePkg/Sample1.java
@@ -3,6 +3,7 @@ package samplePkg;
 public class Sample1 {
 	public static void main(String args[]) {
 		System.out.println("Hiiii....This is git.....");
+		System.out.println("Hiiii....git hub deskstop.....");
 	}
 
 }
